f3178757ed4b9f5291bc6dedc1c006476c7e7543
Marcel Rød <marcelroed@gmail.com>
2024-04-27 17:36:36 +0000

    cs336-basics/

--- cs336-systems/tests/adapters.py
+++ cs336-systems/tests/adapters.py
@@ -2,6 +2,7 @@
 from __future__ import annotations
 
 from typing import Type
+from cs336_systems.rmsnorm import RMSNormAutogradFunction, RMSNormAutogradFunctionTriton, RMSNormTriton, compute_rmsnorm_backward_g, compute_rmsnorm_backward_x
 
 import torch
 
@@ -16,7 +17,7 @@ def get_rmsnorm_autograd_function_pytorch() -> Type:
         A class object (not an instance of the class)
     """
     # For example: return MyRMSNormAutogradFunctionClass
-    raise NotImplementedError
+    return RMSNormAutogradFunction
 
 
 def get_rmsnorm_autograd_function_triton() -> Type:
@@ -32,7 +33,7 @@ def get_rmsnorm_autograd_function_triton() -> Type:
         A class object (not an instance of the class)
     """
     # For example: return MyTritonRMSNormAutogradFunctionClass
-    raise NotImplementedError
+    return RMSNormAutogradFunctionTriton
 
 
 def rmsnorm_backward_g_pytorch(
@@ -53,7 +54,7 @@ def rmsnorm_backward_g_pytorch(
     Returns:
         Gradient of the loss with respect to g. Shape: (H,)
     """
-    raise NotImplementedError
+    return compute_rmsnorm_backward_g(grad_output, x, g)
 
 
 def rmsnorm_backward_x_pytorch(
@@ -74,7 +75,7 @@ def rmsnorm_backward_x_pytorch(
     Returns:
         Gradient of the loss with respect to x. Shape: (*, H)
     """
-    raise NotImplementedError
+    return compute_rmsnorm_backward_x(grad_output, x, g)
 
 
 def get_ddp_individual_parameters(module: torch.nn.Module) -> torch.nn.Module:
