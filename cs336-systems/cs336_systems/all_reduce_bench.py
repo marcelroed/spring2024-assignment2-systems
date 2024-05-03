fd54f8ceff9b37686963ca50d827cdca7fe50293
Marcel Rød <marcelroed@gmail.com>
2024-05-03 18:57:27 +0000

    Set device for time tensor

--- cs336-systems/cs336_systems/all_reduce_bench.py
+++ cs336-systems/cs336_systems/all_reduce_bench.py
@@ -52,7 +52,7 @@ def distributed_bench(rank, world_size, backend, device_type='cpu'):
         if device != 'cpu':
             torch.cuda.synchronize(device=device)
         end = timer()
-        mean_time = torch.tensor(start - end)
+        mean_time = torch.tensor(start - end, device=device)
         dist.all_reduce(mean_time, op=dist.ReduceOp.AVG, async_op=False)
         if rank == 0:
             print(f'[{backend.upper()} + {device_type.upper()}], [{format_bytes(tensor_size * 4)}], [${world_size}$], [${mean_time:.2e}$],')
